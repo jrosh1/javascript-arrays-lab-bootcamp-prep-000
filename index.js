57b9090970e273478e1033de0cb36912f33a19d8
Jacob Roshgadol <jacobiroshgadol@gmail.com>
2017-12-28 01:50:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,12 +22,12 @@ function destructivelyRemoveFirstKitten(){
 }
 
 function appendKitten(name){
-  kittens = [...kittens, "name"]
+  kittens = [...kittens, name]
   return kittens
 }
 
 function prependKitten(name){
-  kittens = ["name", ...kittens]
+  kittens = [name, ...kittens]
   return kittens
 }
 
