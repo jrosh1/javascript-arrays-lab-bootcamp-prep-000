e0c88e4b5c7fb19d4f82aa26a45c6093a5cf11e0
Jacob Roshgadol <jacobiroshgadol@gmail.com>
2017-12-28 01:40:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+kittens = ["Milo" , "Otis" , "Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(array, name){
+  array.pop(name)
+  return array
+}
+
+function destructivelyRemoveFirstKitten(array){
+  array.slice(1)
+  return array
+}
+
+function appendKitten(kittens, name){
+  kittens = [...kittens, "name"]
+  return kittens
+}
+
+function prependKitten(kittens, name){
+  kittens = ["name", ...kittens]
+  return kittens
+}
+
+function removeFirstKitten(kittens){
+  kittens = kittens.slice(1)
+  return kittens
+}
+
+function removeLastKitten(kittens){
+  kittens = kittens.slice(0, kittens.length-1)
+  return kittens
+}
