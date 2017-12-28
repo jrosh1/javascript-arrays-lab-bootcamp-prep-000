4465694cd85ed50a943ae87fc6b05cbd4ce4a419
Jacob Roshgadol <jacobiroshgadol@gmail.com>
2017-12-28 01:49:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ function prependKitten(name){
   return kittens
 }
 
-function removeFirstKitten(kittens){
+function removeFirstKitten(){
   kittens = kittens.slice(1)
   return kittens
 }
 
-function removeLastKitten(kittens){
+function removeLastKitten(){
   kittens = kittens.slice(0, kittens.length-1)
   return kittens
 }
